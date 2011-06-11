40bce992c6ca0ae63e68dd49ce94419c56a0cfe9
achellies@hotmail.com <achellies@hotmail.com@bd6d7149-f35c-297f-8297-5363c4919487>
2011-06-11 10:00:52 +0000

    update

--- trunk/trunk/duibrwoser/UIWebkit.cpp
+++ trunk/trunk/duibrwoser/UIWebkit.cpp
@@ -25,7 +25,10 @@ namespace DuiLib {
 CWebkitUI::CWebkitUI()
 : bitmap_bits_(NULL)
 , did_first_layout_(false)
-{}
+{
+    last_mouse_point_.x = 0;
+    last_mouse_point_.y = 0;
+}
 
 CWebkitUI::~CWebkitUI()
 {
@@ -45,6 +48,104 @@ void CWebkitUI::SetEARasterAndView(IEARaster* raster, View* view)
 {
 	raster_ = raster;
 	view_ = view;
+}
+
+void CWebkitUI::DoEvent(TEventUI& event)
+{
+    if( !IsMouseEnabled() && event.Type > UIEVENT__MOUSEBEGIN && event.Type < UIEVENT__MOUSEEND ) {
+        if( m_pParent != NULL ) m_pParent->DoEvent(event);
+        else CControlUI::DoEvent(event);
+        return;
+    }
+
+    if( event.Type == UIEVENT_SETFOCUS ) 
+    {
+        view_->OnFocusChangeEvent(true);
+    }
+    if( event.Type == UIEVENT_KILLFOCUS ) 
+    {
+        view_->OnFocusChangeEvent(false);
+    }
+    if( event.Type == UIEVENT_CHAR )
+    {
+        KeyboardEvent keyEvent = {0};
+        keyEvent.mId = event.chKey;
+        keyEvent.mbChar = true;
+        keyEvent.mbDepressed = false;
+        keyEvent.mbShift = ((event.wKeyState & MK_SHIFT) != 0);
+        keyEvent.mbControl = ((event.wKeyState & MK_CONTROL) != 0);
+        keyEvent.mbAlt = ((event.wKeyState & MK_ALT) != 0);
+        view_->OnKeyboardEvent(keyEvent);
+    }
+    if( event.Type == UIEVENT_KEYDOWN )
+    {
+        KeyboardEvent keyEvent = {0};
+        keyEvent.mId = event.chKey;
+        keyEvent.mbChar = false;
+        keyEvent.mbDepressed = true;
+        keyEvent.mbShift = ((event.wKeyState & MK_SHIFT) != 0);
+        keyEvent.mbControl = ((event.wKeyState & MK_CONTROL) != 0);
+        keyEvent.mbAlt = ((event.wKeyState & MK_ALT) != 0);
+        view_->OnKeyboardEvent(keyEvent);
+    }
+    if( event.Type == UIEVENT_KEYUP )
+    {
+        KeyboardEvent keyEvent = {0};
+        keyEvent.mId = event.chKey;
+        keyEvent.mbChar = false;
+        keyEvent.mbDepressed = false;
+        keyEvent.mbShift = ((event.wKeyState & MK_SHIFT) != 0);
+        keyEvent.mbControl = ((event.wKeyState & MK_CONTROL) != 0);
+        keyEvent.mbAlt = ((event.wKeyState & MK_ALT) != 0);
+        view_->OnKeyboardEvent(keyEvent);
+    }
+    if( event.Type == UIEVENT_BUTTONDOWN || event.Type == UIEVENT_DBLCLICK || event.Type == UIEVENT_BUTTONUP )
+    {
+        MouseButtonEvent mouseEvent = {0};
+        mouseEvent.mX = event.ptMouse.x - m_rcItem.left;
+        mouseEvent.mY = event.ptMouse.y - m_rcItem.top;
+        mouseEvent.mId = kMouseLeft;
+        if( event.Type == UIEVENT_BUTTONDOWN || event.Type == UIEVENT_DBLCLICK )
+            mouseEvent.mbDepressed = true;
+        mouseEvent.mbShift = ((event.wKeyState & MK_SHIFT) != 0);
+        mouseEvent.mbControl = ((event.wKeyState & MK_CONTROL) != 0);
+        mouseEvent.mbAlt = ((event.wKeyState & MK_ALT) != 0);
+        view_->OnMouseButtonEvent(mouseEvent);
+        return;
+    }
+    if( event.Type == UIEVENT_MOUSEMOVE )
+    {
+        MouseMoveEvent mouseEvent = {0};
+        mouseEvent.mX = event.ptMouse.x - m_rcItem.left;
+        mouseEvent.mY = event.ptMouse.y - m_rcItem.top;
+        mouseEvent.mDX = event.ptMouse.x - last_mouse_point_.x;
+        mouseEvent.mDY = event.ptMouse.y - last_mouse_point_.y;
+        mouseEvent.mbShift = ((event.wKeyState & MK_SHIFT) != 0);
+        mouseEvent.mbControl = ((event.wKeyState & MK_CONTROL) != 0);
+        mouseEvent.mbAlt = ((event.wKeyState & MK_ALT) != 0);
+        view_->OnMouseMoveEvent(mouseEvent);
+        last_mouse_point_.x = mouseEvent.mX;
+        last_mouse_point_.y = mouseEvent.mY;
+        return;
+    }
+    if( event.Type == UIEVENT_SCROLLWHEEL )
+    {
+		MouseWheelEvent mouseEvent = {0};
+		mouseEvent.mX = event.ptMouse.x - m_rcItem.left;
+		mouseEvent.mY = event.ptMouse.y - m_rcItem.top;
+
+		// default value is 3
+		int uLineDelta = 3;
+		SystemParametersInfo(SPI_GETWHEELSCROLLLINES, 0, &uLineDelta, 0);
+
+        if( LOWORD(event.wParam) == SB_LINEUP ) mouseEvent.mLineDelta = uLineDelta;
+        else mouseEvent.mLineDelta = -uLineDelta;
+        mouseEvent.mbShift = ((event.wKeyState & MK_SHIFT) != 0);
+        mouseEvent.mbControl = ((event.wKeyState & MK_CONTROL) != 0);
+        mouseEvent.mbAlt = ((event.wKeyState & MK_ALT) != 0);
+        view_->OnMouseWheelEvent(mouseEvent);
+        return;
+    }
 }
 
 void CWebkitUI::DoPaint(void* ctx, const RECT& rcPaint)
@@ -67,7 +168,7 @@ void CWebkitUI::DoPaint(void* ctx, const RECT& rcPaint)
 	bi.bmiHeader.biXPelsPerMeter= 0;
 	bi.bmiHeader.biYPelsPerMeter= 0;
 	bi.bmiHeader.biClrUsed		= 0;
-	bi.bmiHeader.biClrImportant	= 0;
+	bi.bmiHeader.biClrImportant	= 0;	
 
 	if (bitmap_bits_ == NULL)
 	{
