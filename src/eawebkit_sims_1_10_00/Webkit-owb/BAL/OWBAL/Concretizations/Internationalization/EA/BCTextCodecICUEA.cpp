4ceab13e0a499671ab8c674f7368402db7686615
achellies <achellies@hotmail.com>
2011-06-14 02:16:56 +0000

    针对gb2312编码的网页（例如百度首页等）EAWebkit在裁剪ICU时把GBK以及GB2312编码的encode和decode都给去除, 默认官网的webkit采用ICU库来进行转化，而EA为了裁剪Webkit使之变小把ICU去除了，而ICU转换的功能则由自己编码实现，但EAWebkit没有添加GBk、GB2312的编码，所以需要自己来添加，方案有多个，可以使用QT Port中的编码转换, 只需在buildBaseTextCodecMaps 和extendTextCodecMaps把qt的编码器注册进去就可以了(这样会是EAWebkit变大)，也可以使用windows 系统提供的MultiByteToWideChar来进行。

--- src/eawebkit_sims_1_10_00/Webkit-owb/BAL/OWBAL/Concretizations/Internationalization/EA/BCTextCodecICUEA.cpp
+++ src/eawebkit_sims_1_10_00/Webkit-owb/BAL/OWBAL/Concretizations/Internationalization/EA/BCTextCodecICUEA.cpp
@@ -53,9 +53,126 @@ using std::auto_ptr;
 using std::min;
 
 #include <EAIO/FnEncode.h>
+#if defined(_WIN32)
+#include <Windows.h>
+#endif
 
 namespace OWBAL {
 
+#if defined(_WIN32)
+void UTF_8ToUnicode(wchar_t* pOut,char *pText)
+{
+	char* uchar = (char *)pOut;
+
+	uchar[1] = ((pText[0] & 0x0F) << 4) + ((pText[1] >> 2) & 0x0F);
+
+	uchar[0] = ((pText[1] & 0x03) << 6) + (pText[2] & 0x3F);
+	return;
+}
+
+void UnicodeToUTF_8(char* pOut,wchar_t* pText)
+{
+	// 注意 WCHAR高低字的顺序,低字节在前，高字节在后
+	char* pchar = (char *)pText;
+	
+	pOut[0] = (0xE0 | ((pchar[1] & 0xF0) >> 4));
+	pOut[1] = (0x80 | ((pchar[1] & 0x0F) << 2)) + ((pchar[0] & 0xC0) >> 6);
+	pOut[2] = (0x80 | (pchar[0] & 0x3F));
+
+	return;
+}
+
+void UnicodeToGB2312(char* pOut,wchar_t uData)
+{
+	WideCharToMultiByte(CP_ACP,NULL,&uData,1,pOut,sizeof(wchar_t),NULL,NULL);
+	return;
+}
+
+void Gb2312ToUnicode(wchar_t* pOut,char *gbBuffer)
+{
+	::MultiByteToWideChar(CP_ACP,MB_PRECOMPOSED,gbBuffer,2,pOut,1);
+	return;
+}
+
+void GB2312ToUTF_8(CString& pOut,char *pText, int pLen)
+{
+	char buf[4];
+	int nLength = pLen* 3;
+	char* rst = new char[nLength];
+	
+	memset(buf,0,4);
+	memset(rst,0,nLength);
+	
+	int i = 0;
+	int j = 0;
+	while(i < pLen)
+	{
+		//如果是英文直接复制就可以
+		if( *(pText + i) >= 0)
+		{
+			rst[j++] = pText[i++];
+		}  
+		else
+		{
+			wchar_t pbuffer;
+			Gb2312ToUnicode(&pbuffer,pText+i);
+			UnicodeToUTF_8(buf,&pbuffer);
+			
+			unsigned short int tmp = 0;
+			tmp = rst[j] = buf[0];
+			tmp = rst[j+1] = buf[1];
+			tmp = rst[j+2] = buf[2];
+			
+			j += 3;
+			i += 2;
+		}
+	}
+	
+	rst[j] = '\0';
+
+	//返回结果
+	pOut = rst;
+	delete []rst;
+	return;
+}
+
+void UTF_8ToGB2312(CString &pOut, char *pText, int pLen)
+{
+	char * newBuf = new char[pLen];
+	char Ctemp[4];
+	
+	memset(Ctemp,0,4);
+	
+	int i =0;
+	int j = 0;
+	
+	while(i < pLen)
+	{
+		if(pText[i] > 0)
+		{
+			newBuf[j++] = pText[i++];
+		}
+		else
+		{
+			WCHAR Wtemp;
+			UTF_8ToUnicode(&Wtemp,pText + i);
+			UnicodeToGB2312(Ctemp,Wtemp);
+			
+			newBuf[j] = Ctemp[0];
+			newBuf[j + 1] = Ctemp[1];
+			
+			i += 3;
+			j += 2;
+		}
+	}
+	
+	newBuf[j] = '\0';
+	
+	pOut = newBuf;
+	delete []newBuf;
+	return;
+}
+#endif
 
 static auto_ptr<TextCodec> newTextCodecICU(const TextEncoding& encoding, const void*)
 {
@@ -367,10 +484,10 @@ void TextCodecICU::registerExtendedEncodingNames(EncodingNameRegistrar registrar
         // registrar("CP936", "GBK");
         // registrar("MS936", "GBK");
         // registrar("windows-936", "GBK");
-        // registrar("GBK", "GBK");
+         registrar("GBK", "GBK");
         // registrar("ibm-1383_P110-1999", "GBK");
         // registrar("ibm-1383", "GBK");
-        // registrar("GB2312", "GBK");
+         registrar("GB2312", "GBK");
         // registrar("csGB2312", "GBK");
         // registrar("cp1383", "GBK");
         // registrar("1383", "GBK");
@@ -916,7 +1033,7 @@ void TextCodecICU::registerExtendedCodecs(TextCodecRegistrar registrar)
         // registrar("Extended_UNIX_Code_Packed_Format_for_Japanese", newTextCodecICU, 0);
         // registrar("Big5", newTextCodecICU, 0);
         // registrar("Big5-HKSCS", newTextCodecICU, 0);
-        // registrar("GBK", newTextCodecICU, 0);
+         registrar("GBK", newTextCodecICU, 0);
         // registrar("GB2312", newTextCodecICU, 0);
         // registrar("GB_2312-80", newTextCodecICU, 0);
         // registrar("EUC-KR", newTextCodecICU, 0);
@@ -999,9 +1116,7 @@ TextCodecICU::TextCodecICU(const TextEncoding& encoding)
     #if _DEBUG
         memset(m_bufferedBytes, 0, sizeof(m_bufferedBytes));
     #endif
-}
-
-
+}
 // This function can be called multiple times successively, with flush being false for
 // each time but the last. It's possible that the data may be such that a multibyte
 // character straddles the division between two blocks given in successive calls.
@@ -1065,6 +1180,15 @@ String TextCodecICU::decode(const char* bytes, size_t length, bool flush, bool /
         UChar*       pUsedBuffer   = ((length * sizeof(UChar)) < kMaxStackSize) ? (UChar*)alloca(length * sizeof(UChar)) : (pMallocBuffer = (UChar*)fastMalloc(length * sizeof(UChar)));
         size_t       j = 0;
 
+#if defined(_WIN32)
+		if (strcmp(m_encoding.name(), "GBK") == 0 || stricmp(m_encoding.name(), "gb18030") == 0)
+		{
+			j = ::MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, bytes, length, pUsedBuffer, 0);
+			::MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, bytes, length, pUsedBuffer, length);
+		}
+		else
+#endif
+		{
         for(size_t i = 0; i<length; ++i, ++j)
         {
             if (((unsigned char)bytes[i] & 240) == 240)  // If we appear to have a 4-byte UTF8 sequence...
@@ -1136,6 +1260,7 @@ String TextCodecICU::decode(const char* bytes, size_t length, bool flush, bool /
             else
                 pUsedBuffer[j] = (unsigned char)bytes[i];
         }
+		}
 
         resultString.append(pUsedBuffer, j);
 
