118378282d8d939a225b2e5f43344a81f9041ee6
achellies <achellies@hotmail.com>
2011-06-22 01:49:10 +0000

    修正本地文件的路径中包含中文无法打开的bug，增加url encode 和 url deocode

--- src/duibrowser/frame.cpp
+++ src/duibrowser/frame.cpp
@@ -193,6 +193,79 @@ std::string MainFrame::GetUserAgent()
 	return user_agent = szBuf;
 }
 
+std::string MainFrame::UrlEncode(const std::string& szToEncode)
+{
+	std::string src = szToEncode;
+	char hex[] = "0123456789ABCDEF";
+	string dst;
+
+	for (size_t i = 0; i < src.size(); ++i)
+	{
+		unsigned char cc = src[i];
+		if (isascii(cc))
+		{
+			if (cc == ' ')
+			{
+				dst += "%20";
+			}
+			else
+				dst += cc;
+		}
+		else
+		{
+			unsigned char c = static_cast<unsigned char>(src[i]);
+			dst += '%';
+			dst += hex[c / 16];
+			dst += hex[c % 16];
+		}
+	}
+	return dst;
+}
+
+
+std::string MainFrame::UrlDecode(const std::string& szToDecode)
+{
+	std::string result;
+	int hex = 0;
+	for (size_t i = 0; i < szToDecode.length(); ++i)
+	{
+		switch (szToDecode[i])
+		{
+		case '+':
+			result += ' ';
+			break;
+		case '%':
+			if (isxdigit(szToDecode[i + 1]) && isxdigit(szToDecode[i + 2]))
+			{
+				std::string hexStr = szToDecode.substr(i + 1, 2);
+				hex = strtol(hexStr.c_str(), 0, 16);
+				//字母和数字[0-9a-zA-Z]、一些特殊符号[$-_.+!*'(),] 、以及某些保留字[$&+,/:;=?@]
+				//可以不经过编码直接用于URL
+				if (!((hex >= 48 && hex <= 57) ||	//0-9
+					(hex >=97 && hex <= 122) ||	//a-z
+					(hex >=65 && hex <= 90) ||	//A-Z
+					//一些特殊符号及保留字[$-_.+!*'(),]  [$&+,/:;=?@]
+					hex == 0x21 || hex == 0x24 || hex == 0x26 || hex == 0x27 || hex == 0x28 || hex == 0x29
+					|| hex == 0x2a || hex == 0x2b|| hex == 0x2c || hex == 0x2d || hex == 0x2e || hex == 0x2f
+					|| hex == 0x3A || hex == 0x3B|| hex == 0x3D || hex == 0x3f || hex == 0x40 || hex == 0x5f
+					))
+				{
+					result += char(hex);
+					i += 2;
+				}
+				else result += '%';
+			}else {
+				result += '%';
+			}
+			break;
+		default:
+			result += szToDecode[i];
+			break;
+		}
+	}
+	return result;
+}
+
 LPCTSTR MainFrame::GetWindowClassName() const
 {
 	return _T("DuiBrowserMainFrame");
@@ -462,8 +535,6 @@ void MainFrame::OnPrepare(TNotifyUI& msg)
 	{
 		paint_manager_.SetTimer(app_title, kViewTickTimerId, kViewTickTimerElapse);
 		webkit_control->SetEARasterAndView(raster_, view_);
-
-		//view_->SetURI("file:///E:/Webkit/chapter01/chapter01.html");
 		view_->SetURI(kHomeUrl);
 	}
 }
@@ -518,7 +589,8 @@ void MainFrame::Notify(TNotifyUI& msg)
 			{}
 			else if ((input_url.find(_T("http://")) == tString::npos) && (input_url.find(_T("https://")) == tString::npos))
 				input_url = _T("http://") + input_url;
-			view_->SetURI(StringConvertor::WideToUtf8(input_url.c_str()));
+			std::string encode_url = UrlEncode(StringConvertor::WideToUtf8(input_url.c_str()));			
+			view_->SetURI(encode_url.c_str());
 		}
 	}
 	else if (_tcsicmp(msg.sType, _T("click")) == 0)
@@ -628,7 +700,9 @@ bool MainFrame::LoadUpdate(LoadInfo& load_info)
 		_tcsicmp(webkit_->GetCharacters(load_info.mURI), navigating_url_.c_str()) != 0)
 	{
 		navigating_url_ = webkit_->GetCharacters(load_info.mURI);
-		address_edit->SetText(navigating_url_.c_str());
+
+		std::string decode_url = UrlDecode(StringConvertor::WideToUtf8(navigating_url_.c_str()));
+		address_edit->SetText(StringConvertor::Utf8ToWide(decode_url.c_str()));
 	}
 
 	if (load_info.mLET == kLETLoadStarted)
